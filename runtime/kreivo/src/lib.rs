60a81c4b7dc803ad23269b9b5f62fc142c334118
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2025-03-01 22:11:40 -0500

    feat(kreivo-runtime): bump version

--- runtime/kreivo/src/lib.rs
+++ runtime/kreivo/src/lib.rs
@@ -126,10 +126,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kreivo-parachain"),
 	impl_name: create_runtime_str!("kreivo-parachain"),
 	authoring_version: 1,
-	spec_version: 116,
+	spec_version: 117,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 10,
+	transaction_version: 11,
 	state_version: 1,
 };
 
