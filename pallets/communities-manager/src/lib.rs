a23460c69de5d5712dd6f1d72595f27fae3e4b45
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2025-04-23 18:25:55 -0500

    feat(pallet-communities-manager): add `GenesisConfig`/`GenesisBuilder` + remove `sp-std`

--- pallets/communities-manager/src/lib.rs
+++ pallets/communities-manager/src/lib.rs
@@ -1,18 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub use pallet::*;
+extern crate alloc;
 
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
-
-#[cfg(test)]
-pub(crate) mod mock;
-#[cfg(test)]
-mod tests;
-
-pub mod weights;
-pub use weights::*;
-
+use alloc::{string::String, vec::Vec};
 use frame_contrib_traits::{gas_tank::MakeTank, tracks::MutateTracks};
 use frame_support::{
 	pallet_prelude::*,
@@ -24,8 +14,8 @@ use frame_support::{
 };
 use frame_system::pallet_prelude::{BlockNumberFor, OriginFor};
 use pallet_communities::{
-	types::{AccountIdOf, CommunityIdOf, DecisionMethodFor, NativeBalanceOf, PalletsOriginOf, RuntimeOriginFor},
-	Origin as CommunityOrigin,
+	AccountIdOf, AssetIdOf, CommunityIdOf, DecisionMethodFor, NativeBalanceOf, Origin as CommunityOrigin,
+	PalletsOriginOf, RuntimeOriginFor,
 };
 use pallet_nfts::CollectionConfig;
 use pallet_referenda::{TrackInfo, TracksInfo};
@@ -34,6 +24,19 @@ use sp_runtime::{
 	str_array,
 	traits::{Get, StaticLookup},
 };
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[cfg(test)]
+pub(crate) mod mock;
+#[cfg(test)]
+mod tests;
+
+pub use pallet::*;
+
+pub mod weights;
+pub use weights::*;
 
 type TrackInfoOf<T> = TrackInfo<NativeBalanceOf<T>, BlockNumberFor<T>>;
 
@@ -46,6 +49,7 @@ pub struct TankConfig<Weight, BlockNumber> {
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
+	use frame_support::DefaultNoBound;
 	use parity_scale_codec::HasCompact;
 
 	type CommunityName = BoundedVec<u8, ConstU32<25>>;
@@ -53,7 +57,10 @@ pub mod pallet {
 	/// Configure the pallet by specifying the parameters and types on which it
 	/// depends.
 	#[pallet::config]
-	pub trait Config: frame_system::Config + pallet_communities::Config {
+	pub trait Config: frame_system::Config + pallet_communities::Config
+	where
+		AssetIdOf<Self>: MaybeSerializeDeserialize,
+	{
 		/// Because this pallet emits events, it depends on the runtime's
 		/// definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
@@ -90,7 +97,7 @@ pub mod pallet {
 
 		type CreateMembershipsOrigin: EnsureOrigin<OriginFor<Self>>;
 
-		type MembershipId: Parameter + Decode + Incrementable + HasCompact;
+		type MembershipId: Parameter + Decode + Incrementable + HasCompact + MaybeSerializeDeserialize;
 
 		type MembershipsManagerCollectionId: Get<CommunityIdOf<Self>>;
 
@@ -111,6 +118,72 @@ pub mod pallet {
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
+
+	/// A genesis community info.
+	pub type GenesisCommunityOf<T> = (
+		// community_id,im
+		CommunityIdOf<T>,
+		// name
+		String,
+		// admin
+		AccountIdOf<T>,
+		// maybe_decision_method
+		Option<DecisionMethodFor<T>>,
+		// maybe_rank
+		Option<u16>,
+	);
+
+	/// A genesis membership info.
+	pub type GenesisMembershipOf<T> = (
+		// starting_at
+		<T as Config>::MembershipId,
+		// amount
+		u16,
+		// price
+		NativeBalanceOf<T>,
+		// tank_config,
+		(Option<Weight>, Option<BlockNumberFor<T>>),
+		// maybe_expiration
+		Option<BlockNumberFor<T>>,
+	);
+
+	#[pallet::genesis_config]
+	#[derive(DefaultNoBound)]
+	pub struct GenesisConfig<T: Config> {
+		/// A list of initial communities, with the most basic settings.
+		pub communities: Vec<GenesisCommunityOf<T>>,
+		pub memberships: Vec<GenesisMembershipOf<T>>,
+	}
+
+	#[pallet::genesis_build]
+	impl<T: Config> BuildGenesisConfig for GenesisConfig<T> {
+		fn build(&self) {
+			for (community_id, name, admin, maybe_decision_method, maybe_rank) in &self.communities {
+				Pallet::<T>::try_register(
+					None, // genesis communities are given for free :)
+					*community_id,
+					name.as_str(),
+					admin,
+					maybe_decision_method.clone(),
+					None, // use the default track info. Let the community set the track later.
+					*maybe_rank,
+				)
+				.unwrap();
+			}
+
+			for (starting_at, amount, price, tank_config, maybe_expiration) in &self.memberships {
+				let (capacity, periodicity) = *tank_config;
+				Pallet::<T>::try_create_memberships(
+					starting_at.clone(),
+					*amount,
+					*price,
+					TankConfig { capacity, periodicity },
+					*maybe_expiration,
+				)
+				.unwrap()
+			}
+		}
+	}
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/main-docs/build/events-errors/
@@ -153,50 +226,15 @@ pub mod pallet {
 			maybe_decision_method: Option<DecisionMethodFor<T>>,
 			maybe_track_info: Option<TrackInfoOf<T>>,
 		) -> DispatchResult {
-			let maybe_deposit = T::RegisterOrigin::ensure_origin(origin)?;
-
-			let community_name = core::str::from_utf8(&name).map_err(|_| Error::<T>::InvalidCommunityName)?;
-
-			let first_admin_account_id = T::Lookup::lookup(first_admin)?;
-			let admin_origin = frame_system::Origin::<T>::Signed(first_admin_account_id);
-
-			// Register first to check if community exists
-			pallet_communities::Pallet::<T>::register(&admin_origin.clone().into(), &community_id, maybe_deposit)?;
-
-			if let Some(decision_method) = maybe_decision_method {
-				pallet_communities::Pallet::<T>::set_decision_method(
-					admin_origin.into(),
-					community_id,
-					decision_method,
-				)?;
-			}
-
-			let community_account = pallet_communities::Pallet::<T>::community_account(&community_id);
-
-			// Create memberships collection for community
-			T::CreateCollection::create_collection_with_id(
+			Self::try_register(
+				T::RegisterOrigin::ensure_origin(origin)?,
 				community_id,
-				&community_account,
-				&community_account,
-				&CollectionConfig {
-					settings: Default::default(),
-					max_supply: None,
-					mint_settings: Default::default(),
-				},
-			)?;
-
-			// Create governance track for community
-			let community_origin: RuntimeOriginFor<T> = CommunityOrigin::<T>::new(community_id).into();
-			T::Tracks::insert(
-				community_id,
-				maybe_track_info.unwrap_or_else(|| Self::default_tack(community_name)),
-				community_origin.into_caller(),
-			)?;
-			// Induct community at Kreivo Governance with rank 0
-			T::RankedCollective::induct(&community_account)?;
-
-			Self::deposit_event(Event::<T>::CommunityRegistered { id: community_id });
-			Ok(())
+				core::str::from_utf8(&name).map_err(|_| Error::<T>::InvalidCommunityName)?,
+				&T::Lookup::lookup(first_admin)?,
+				maybe_decision_method,
+				maybe_track_info,
+				None,
+			)
 		}
 
 		#[pallet::weight(<T as Config>::WeightInfo::create_memberships((*amount).into()))]
@@ -212,49 +250,7 @@ pub mod pallet {
 			ensure!(amount <= 1024u16, Error::<T>::CreatingTooManyMemberships);
 			T::CreateMembershipsOrigin::ensure_origin(origin.clone())?;
 
-			let collection_id = &T::MembershipsManagerCollectionId::get();
-			let mut id = starting_at.clone();
-			let mut minted = 0u32;
-			for _ in 0..amount {
-				T::CreateMemberships::mint_into(
-					collection_id,
-					&id,
-					&T::MembershipsManagerOwner::get(),
-					&Default::default(),
-					true,
-				)?;
-
-				Self::do_set_gas_tank(&(*collection_id, id.clone()), &tank_config)?;
-
-				if let Some(expiration) = maybe_expiration {
-					T::CreateMemberships::set_typed_attribute(
-						collection_id,
-						&id,
-						&b"membership_expiration",
-						&expiration,
-					)?;
-				}
-
-				T::CreateMemberships::set_price(
-					&T::MembershipsManagerCollectionId::get(),
-					&id,
-					&T::MembershipsManagerOwner::get(),
-					Some(price),
-					None,
-				)?;
-				if let Some(next_id) = id.increment() {
-					id = next_id;
-					minted += 1;
-				} else {
-					break;
-				}
-			}
-
-			Self::deposit_event(Event::<T>::MembershipsCreated {
-				starting_at,
-				amount: minted,
-			});
-			Ok(())
+			Self::try_create_memberships(starting_at.clone(), amount, price, tank_config, maybe_expiration)
 		}
 
 		#[pallet::call_index(2)]
@@ -268,40 +264,140 @@ pub mod pallet {
 			Self::do_set_gas_tank(&(community_id, membership_id), &config)
 		}
 	}
+}
 
-	impl<T: Config> Pallet<T> {
-		#[inline]
-		pub(crate) fn do_set_gas_tank(
-			tank_id: &(CommunityIdOf<T>, <T as Config>::MembershipId),
-			config: &TankConfig<Weight, BlockNumberFor<T>>,
-		) -> DispatchResult {
-			let TankConfig { capacity, periodicity } = config;
-			T::MakeTank::make_tank(tank_id, *capacity, *periodicity)?;
+impl<T: Config> Pallet<T> {
+	pub(crate) fn try_register(
+		maybe_deposit: Option<(NativeBalanceOf<T>, T::AccountId, T::AccountId)>,
+		community_id: CommunityIdOf<T>,
+		community_name: &str,
+		admin: &T::AccountId,
+		maybe_decision_method: Option<DecisionMethodFor<T>>,
+		maybe_track_info: Option<TrackInfoOf<T>>,
+		maybe_rank: Option<u16>,
+	) -> DispatchResult {
+		let admin_origin = frame_system::Origin::<T>::Signed(admin.clone());
 
-			Ok(())
+		// Register first to check if community exists
+		pallet_communities::Pallet::<T>::register(&admin_origin.clone().into(), &community_id, maybe_deposit)?;
+
+		if let Some(decision_method) = maybe_decision_method {
+			pallet_communities::Pallet::<T>::set_decision_method(admin_origin.into(), community_id, decision_method)?;
 		}
 
-		fn default_tack(name: &str) -> TrackInfoOf<T> {
-			use sp_runtime::Perbill;
-			TrackInfo {
-				name: str_array(name),
-				max_deciding: 1,
-				decision_deposit: 0u8.into(),
-				prepare_period: 1u8.into(),
-				decision_period: u8::MAX.into(),
-				confirm_period: 1u8.into(),
-				min_enactment_period: 1u8.into(),
-				min_approval: pallet_referenda::Curve::LinearDecreasing {
-					length: Perbill::from_percent(100),
-					floor: Perbill::from_percent(50),
-					ceil: Perbill::from_percent(100),
-				},
-				min_support: pallet_referenda::Curve::LinearDecreasing {
-					length: Perbill::from_percent(100),
-					floor: Perbill::from_percent(0),
-					ceil: Perbill::from_percent(50),
-				},
+		let community_account = pallet_communities::Pallet::<T>::community_account(&community_id);
+
+		// Create memberships collection for community
+		T::CreateCollection::create_collection_with_id(
+			community_id,
+			&community_account,
+			&community_account,
+			&CollectionConfig {
+				settings: Default::default(),
+				max_supply: None,
+				mint_settings: Default::default(),
+			},
+		)?;
+
+		// Create governance track for community
+		let community_origin: RuntimeOriginFor<T> = CommunityOrigin::<T>::new(community_id).into();
+		T::Tracks::insert(
+			community_id,
+			maybe_track_info.unwrap_or_else(|| Self::default_tack(community_name)),
+			community_origin.into_caller(),
+		)?;
+		// Induct community at Kreivo Governance with rank 0
+		T::RankedCollective::induct(&community_account)?;
+		if let Some(rank) = maybe_rank {
+			for _ in 0..rank {
+				T::RankedCollective::promote(&community_account)?;
 			}
+		}
+
+		Self::deposit_event(Event::<T>::CommunityRegistered { id: community_id });
+		Ok(())
+	}
+
+	pub(crate) fn try_create_memberships(
+		starting_at: <T as Config>::MembershipId,
+		amount: u16,
+		price: NativeBalanceOf<T>,
+		tank_config: TankConfig<Weight, BlockNumberFor<T>>,
+		maybe_expiration: Option<BlockNumberFor<T>>,
+	) -> DispatchResult {
+		let collection_id = &T::MembershipsManagerCollectionId::get();
+
+		let mut id = starting_at.clone();
+		let mut minted = 0u32;
+		for _ in 0..amount {
+			T::CreateMemberships::mint_into(
+				collection_id,
+				&id,
+				&T::MembershipsManagerOwner::get(),
+				&Default::default(),
+				true,
+			)?;
+
+			Self::do_set_gas_tank(&(*collection_id, id.clone()), &tank_config)?;
+
+			if let Some(expiration) = maybe_expiration {
+				T::CreateMemberships::set_typed_attribute(collection_id, &id, &b"membership_expiration", &expiration)?;
+			}
+
+			T::CreateMemberships::set_price(
+				&T::MembershipsManagerCollectionId::get(),
+				&id,
+				&T::MembershipsManagerOwner::get(),
+				Some(price),
+				None,
+			)?;
+			if let Some(next_id) = id.increment() {
+				id = next_id;
+				minted += 1;
+			} else {
+				break;
+			}
+		}
+
+		Self::deposit_event(Event::<T>::MembershipsCreated {
+			starting_at,
+			amount: minted,
+		});
+
+		Ok(())
+	}
+
+	#[inline]
+	pub(crate) fn do_set_gas_tank(
+		tank_id: &(CommunityIdOf<T>, <T as Config>::MembershipId),
+		config: &TankConfig<Weight, BlockNumberFor<T>>,
+	) -> DispatchResult {
+		let TankConfig { capacity, periodicity } = config;
+		T::MakeTank::make_tank(tank_id, *capacity, *periodicity)?;
+
+		Ok(())
+	}
+
+	fn default_tack(name: &str) -> TrackInfoOf<T> {
+		use sp_runtime::Perbill;
+		TrackInfo {
+			name: str_array(name),
+			max_deciding: 1,
+			decision_deposit: 0u8.into(),
+			prepare_period: 1u8.into(),
+			decision_period: u8::MAX.into(),
+			confirm_period: 1u8.into(),
+			min_enactment_period: 1u8.into(),
+			min_approval: pallet_referenda::Curve::LinearDecreasing {
+				length: Perbill::from_percent(100),
+				floor: Perbill::from_percent(50),
+				ceil: Perbill::from_percent(100),
+			},
+			min_support: pallet_referenda::Curve::LinearDecreasing {
+				length: Perbill::from_percent(100),
+				floor: Perbill::from_percent(0),
+				ceil: Perbill::from_percent(50),
+			},
 		}
 	}
 }
