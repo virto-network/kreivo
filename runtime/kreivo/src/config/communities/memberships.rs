dff6d92234d9977207b0dd6146365efd842a3322
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2025-05-12 17:12:09 -0500

    fix(kreivo-runtime): simplify `CommunityMemberships` configuration

--- runtime/kreivo/src/config/communities/memberships.rs
+++ runtime/kreivo/src/config/communities/memberships.rs
@@ -11,9 +11,6 @@ use virto_common::MembershipId;
 parameter_types! {
 	pub MembershipsPalletFeatures: PalletFeatures = PalletFeatures::all_enabled();
 	pub const MaxDeadlineDuration: BlockNumber = 12 * 30 * DAYS;
-	pub const MetadataDepositBase: Balance = 0;
-	pub const AttributeDepositBase: Balance = 0;
-	pub const DepositPerByte: Balance = 0;
 }
 
 const WELL_KNOWN_ATTR_KEYS: [&[u8]; 3] = [b"membership_member_rank", b"membership_gas", b"membership_expiration"];
@@ -34,30 +31,25 @@ parameter_types! {
 
 pub type CommunityMembershipsInstance = pallet_nfts::Instance2;
 
-// From https://github.com/polkadot-fellows/runtimes/blob/main/system-parachains/asset-hubs/asset-hub-kusama/src/lib.rs#L810
 impl pallet_nfts::Config<CommunityMembershipsInstance> for Runtime {
 	type RuntimeEvent = RuntimeEvent;
-
 	type CollectionId = CommunityId;
 	type ItemId = MembershipId;
-
 	type Currency = Balances;
 	type ForceOrigin = EnsureRoot<AccountId>;
-	// Ensure only root is allowed to executing `create` calls
+	// Ensure only root is allowed to issue new membership groups.
 	type CreateOrigin = EnsureRootWithSuccess<AccountId, TreasuryAccount>;
 	type Locker = ();
-
 	type CollectionDeposit = ();
 	#[cfg(not(feature = "runtime-benchmarks"))]
 	type ItemDeposit = ();
 	#[cfg(feature = "runtime-benchmarks")]
-	/// When benchmarking, items must have a deposit cost, in order for
-	/// `redeposit` to work.
+	// When benchmarking, items must have a deposit cost, in order for `redeposit`
+	// to work.
 	type ItemDeposit = ConstU128<CENTS>;
-	type MetadataDepositBase = MetadataDepositBase;
-	type AttributeDepositBase = AttributeDepositBase;
-	type DepositPerByte = DepositPerByte;
-
+	type MetadataDepositBase = ();
+	type AttributeDepositBase = ();
+	type DepositPerByte = ();
 	type StringLimit = ConstU32<256>;
 	type KeyLimit = ConstU32<64>;
 	type ValueLimit = ConstU32<256>;
@@ -66,41 +58,11 @@ impl pallet_nfts::Config<CommunityMembershipsInstance> for Runtime {
 	type MaxTips = ConstU32<10>;
 	type MaxDeadlineDuration = MaxDeadlineDuration;
 	type MaxAttributesPerCall = ConstU32<10>;
-	type Features = MembershipsPalletFeatures;
-
+	type Features = ();
 	type OffchainSignature = Signature;
 	type OffchainPublic = <Signature as Verify>::Signer;
 	#[cfg(feature = "runtime-benchmarks")]
-	type Helper = NftsBenchmarksHelper;
-
+	type Helper = ();
 	type WeightInfo = pallet_nfts::weights::SubstrateWeight<Runtime>;
 	type BlockNumberProvider = System;
-}
-
-#[cfg(feature = "runtime-benchmarks")]
-pub struct NftsBenchmarksHelper;
-
-#[cfg(feature = "runtime-benchmarks")]
-use sp_runtime::traits::IdentifyAccount;
-
-#[cfg(feature = "runtime-benchmarks")]
-impl pallet_nfts::BenchmarkHelper<CommunityId, MembershipId, <Signature as Verify>::Signer, AccountId, Signature>
-	for NftsBenchmarksHelper
-{
-	fn collection(id: u16) -> CommunityId {
-		id
-	}
-	fn item(i: u16) -> MembershipId {
-		i.into()
-	}
-	fn signer() -> (sp_runtime::MultiSigner, AccountId) {
-		let public = sp_io::crypto::sr25519_generate(0.into(), None);
-		let account = sp_runtime::MultiSigner::Sr25519(public).into_account();
-		(public.into(), account)
-	}
-	fn sign(signer: &sp_runtime::MultiSigner, message: &[u8]) -> Signature {
-		sp_runtime::MultiSignature::Sr25519(
-			sp_io::crypto::sr25519_sign(0.into(), &signer.clone().try_into().unwrap(), message).unwrap(),
-		)
-	}
 }
