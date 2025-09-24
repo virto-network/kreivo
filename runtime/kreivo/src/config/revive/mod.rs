44192b2eca9ea0461719eeab6e7cb95d9934c43c
Lex Luthor <johan@nulo.network>
2025-09-24 17:46:09 +0200

    Included revive 0.7.0, pending to know if needed a new instance for pallet_assets::Instance3 as done in asset-hub-westend

---
+++ runtime/kreivo/src/config/revive/mod.rs
@@ -0,0 +1,41 @@
+use super::*;
+use pallet_assets::precompiles::{InlineIdConfig, ERC20};
+
+parameter_types! {
+	pub const DepositPerItem: Balance = deposit(1, 0);
+	pub const DepositPerByte: Balance = deposit(0, 1);
+	pub CodeHashLockupDepositPercent: Perbill = Perbill::from_percent(30);
+	pub const ChainId: u64 = 2281;
+}
+
+pub type TrustBackedAssetsInstance = pallet_assets::Instance1;
+// pub type PoolAssetsInstance = pallet_assets::Instance3; // Should be created a new instance for this (Instance3)?
+pub type PoolAssetsInstance = pallet_assets::Instance1;
+use frame_system::EnsureSigned;
+
+impl pallet_revive::Config for Runtime {
+	type Time = Timestamp;
+	type Currency = Balances;
+	type RuntimeEvent = RuntimeEvent;
+	type RuntimeCall = RuntimeCall;
+	type DepositPerItem = DepositPerItem;
+	type DepositPerByte = DepositPerByte;
+	type WeightPrice = pallet_transaction_payment::Pallet<Self>;
+	type WeightInfo = pallet_revive::weights::SubstrateWeight<Self>;
+	type Precompiles = (
+		ERC20<Self, InlineIdConfig<0x120>, TrustBackedAssetsInstance>,
+		ERC20<Self, InlineIdConfig<0x320>, PoolAssetsInstance>,
+	);
+	type AddressMapper = pallet_revive::AccountId32Mapper<Self>;
+	type RuntimeMemory = ConstU32<{ 128 * 1024 * 1024 }>;
+	type PVFMemory = ConstU32<{ 512 * 1024 * 1024 }>;
+	type UnsafeUnstableInterface = ConstBool<false>;
+	type UploadOrigin = EnsureSigned<Self::AccountId>;
+	type InstantiateOrigin = EnsureSigned<Self::AccountId>;
+	type RuntimeHoldReason = RuntimeHoldReason;
+	type CodeHashLockupDepositPercent = CodeHashLockupDepositPercent;
+	type ChainId = ChainId;
+	type NativeToEthRatio = ConstU32<1_000_000>; // 10^(18 - 12) Eth is 10^18, Native is 10^12.
+	type EthGasEncoder = ();
+	type FindAuthor = <Runtime as pallet_authorship::Config>::FindAuthor;
+}
