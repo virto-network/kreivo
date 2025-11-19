61d945187a0029a7ece23722c131caf4cb780ef1
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2025-11-19 12:22:36 -0500

    feat(kreivo-runtime): update dependencies + increase stability by decreasing block production from 12 (500ms) blocks to 8 (750ms) blocks. (#470)

--- runtime/kreivo/src/lib.rs
+++ runtime/kreivo/src/lib.rs
@@ -168,7 +168,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Borrowed("kreivo-parachain"),
 	impl_name: Borrowed("kreivo-parachain"),
 	authoring_version: 1,
-	spec_version: 128,
+	spec_version: 129,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 12,
